b8ec96c88323e3c56b9fd7290ce049cb0739ce3a
SomilKSharma <120346284+SomilKSharma@users.noreply.github.com>
2023-02-02 10:02:50 +0530

    Tests

--- Tests/test_MatchUnmatchGateway.py
+++ Tests/test_MatchUnmatchGateway.py
@@ -1,7 +1,11 @@
+
 import pytest
-from unittest.mock import patch, AsyncMock
+from unittest.mock import patch, AsyncMock, MagicMock
+import redis
+from datetime import datetime
+
 from Gateways.MatchUnmatchGateway import *
-from Tests.Utilities.test_base import async_mock_child
+from Tests.Utilities.test_base import async_mock_child, redis_test_set
 import asyncio
 from unittest.mock import call
 
@@ -21,6 +25,7 @@ async def test_MatchUnmatch_get_match_unmatch_nomatch_for_user_success():
             call(userId='123', childCollectionName='NoMatch')
          ]
         assert result == ["mock data", "mock data", "mock data"]
+  
         
 @pytest.mark.asyncio
 async def test_MatchUnmatch_store_match_or_nomatch_success():
@@ -39,6 +44,7 @@ async def test_MatchUnmatch_store_match_or_nomatch_success():
                                                             match_status=match_status)
             assert result == True
 
+
 @pytest.mark.asyncio
 async def test_MatchUnmatch_store_match_or_nomatch_failure():
     match_status = 'match'
@@ -55,9 +61,196 @@ async def test_MatchUnmatch_store_match_or_nomatch_failure():
             mock_redis_client.sadd.return_value = None
             result = await MatchUnmatch_store_match_or_nomatch(currentUserId=currentUserId, swipedUserId=swipedUserId,match_status=match_status)
             assert result == False
+
+
+
+
+
+@pytest.mark.asyncio
+async def test_MatchUnmatch_send_message_notification_success():
     
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.datetime') as mock_datetime:
+            
+            mock_datetime.today.return_value = datetime(2023, 2, 2)
+            date_str = datetime.today().strftime('%Y%m%d')
+            assert date_str == '20230202'
+            
+            with patch('Gateways.MatchUnmatchGateway.Notification_design_and_multicast') as mock_dm:
+                
+                result= await MatchUnmatch_send_message_notification('2')
+                assert result==True
+                
+@pytest.mark.asyncio
+async def test_MatchUnmatch_send_message_notification_failure():
+    
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.datetime') as mock_datetime:
+            
+            mock_datetime.today.return_value = datetime(2023, 2, 2)
+            date_str = datetime.today().strftime('%Y%m%d')
+            assert date_str == '20230202'
+            
+            with patch('Gateways.MatchUnmatchGateway.Notification_design_and_multicast') as mock_dm:
+                
+                mock_dm.side_effect = Exception("Notification sending failed")
+                result= await MatchUnmatch_send_message_notification('2')
+                assert result==False
+
+
+@pytest.mark.asyncio
+async def test_MatchUnmatch_check_match_between_users_success():
+    swipedUserSwipe = 'Superlikes'
+    currentUserId = "1"
+    swipedUserId = "2"
+    
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:         
+        with patch('Gateways.MatchUnmatchGateway.LikesDislikes_fetch_users_given_swipes') as mock_fetch, patch('Gateways.MatchUnmatchGateway.MatchUnmatch_store_match_or_nomatch') as mock_match, patch('Gateways.MatchUnmatchGateway.MatchUnmatch_write_to_recent_chats') as mock_chats, patch('Gateways.MatchUnmatchGateway.MatchUnmatch_send_message_notification') as mock_not:
+            likesGivenBySwipedUser='1'
+            superlikesGivenBySwipedUser='1'
+            mock_fetch.return_value = (likesGivenBySwipedUser, [], superlikesGivenBySwipedUser)
+
+            result= await MatchUnmatch_check_match_between_users('1','2','Superlikes')
+            assert result==True
+             
+
+
+@pytest.mark.asyncio
+async def test_MatchUnmatch_check_match_between_users_failure():
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.LikesDislikes_fetch_users_given_swipes') as mock_fetch, patch('Gateways.MatchUnmatchGateway.MatchUnmatch_store_match_or_nomatch') as mock_match, patch('Gateways.MatchUnmatchGateway.MatchUnmatch_write_to_recent_chats') as mock_chats, patch('Gateways.MatchUnmatchGateway.MatchUnmatch_send_message_notification') as mock_not:
+            # Simulate the failure scenario
+            mock_fetch.side_effect = Exception("Error while fetching the likes and dislikes")
+
+            # Call the function
+            result = await MatchUnmatch_check_match_between_users(currentUserId='1', swipedUserId='2', currentUserSwipe='Superlikes')
+
+            # Verify that the function returns False
+            assert result == False
 
 
 
+@pytest.mark.asyncio
+async def test_MatchUnmatch_fetch_userdata_from_firebase_or_redis_success():
+    userId='UserId123'
+    childCollectionName='Match'
 
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.redis_client') as mock_redis_client:
+            # Mocking redis_client by fetching the scard value
+            mock_redis_client.scard.return_value = 1
+            with patch('Gateways.MatchUnmatchGateway.MatchUnmatch_fetch_data_from_redis') as matchunnmatch_mock_redis_fetch:
+                matchunnmatch_mock_redis_fetch.return_value = ['UserId234']
+                result = await MatchUnmatch_fetch_userdata_from_firebase_or_redis(userId=userId, childCollectionName=childCollectionName)
+                assert result == ['UserId234']
+            
+            mock_redis_client.scard.return_value = 0
+            mock_async_db.collection.return_value.documen.return_value.collection.return_value.order_by.return_value = ['UserId234']
+            with patch('Gateways.MatchUnmatchGateway.MatchUnmatch_store_match_unmatch_to_redis') as matchunnmatch_store:
+                matchunnmatch_store.return_value = ['UserId234']
+                result = await MatchUnmatch_fetch_userdata_from_firebase_or_redis(userId=userId, childCollectionName=childCollectionName)
+                assert result == ['UserId234']
  
+ 
+
+@pytest.mark.asyncio
+async def test_MatchUnmatch_fetch_userdata_from_firebase_or_redis_failure():
+    userId='UserId123'
+    childCollectionName='Match'
+
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.redis_client') as mock_redis_client:
+            # Mocking redis_client by fetching the scard value
+            mock_redis_client.scard.return_value = 1
+            with patch('Gateways.MatchUnmatchGateway.MatchUnmatch_fetch_data_from_redis') as matchunnmatch_mock_redis_fetch:
+                matchunnmatch_mock_redis_fetch.side_effect = []
+                result = await MatchUnmatch_fetch_userdata_from_firebase_or_redis(userId=0, childCollectionName=childCollectionName)
+                assert result == []
+            
+            mock_redis_client.scard.return_value = 0
+            mock_async_db.collection.return_value.documen.return_value.collection.return_value.order_by.return_value = ['UserId234']
+            with patch('Gateways.MatchUnmatchGateway.MatchUnmatch_fetch_data_from_redis') as matchunnmatch_mock_redis_fetch:
+                matchunnmatch_mock_redis_fetch.side_effect = ['UserId234']
+                assert result == []
+            
+ 
+
+
+@pytest.mark.asyncio
+async def test_MatchUnmatch_fetch_data_from_redis_success():
+    userId='UserId123'
+    childCollectionName='Match'
+
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.redis_client') as mock_redis_client:
+            mock_redis_client.smembers.return_value = ['UserId234']
+            # Mocking redis_client by fetching the scard value
+            profileIds = list(mock_redis_client.smembers.return_value)
+            result = await MatchUnmatch_fetch_data_from_redis(userId=userId, childCollectionName=childCollectionName)
+            assert result == ['UserId234']
+                
+                
+@pytest.mark.asyncio
+async def test_MatchUnmatch_fetch_data_from_redis_failure():
+    userId='UserId123'
+    childCollectionName='Match'
+
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.redis_client') as mock_redis_client:
+            mock_redis_client.smembers.side_effect = Exception("Unablet to fetch")
+            # Mocking redis_client by fetching the scard value
+            profileIds = list(mock_redis_client.smembers.return_value)
+            result = await MatchUnmatch_fetch_data_from_redis(userId=userId, childCollectionName=childCollectionName)
+            assert result == []
+                
+           
+
+def test_MatchUnmatch_calculate_the_match_success():
+    
+    
+    assert MatchUnmatch_calculate_the_match("Likes", "Likes") == "Match"
+    assert MatchUnmatch_calculate_the_match("Likes", "Superlikes") == "Match"
+    assert MatchUnmatch_calculate_the_match("Likes", "Dislikes") == "NoMatch"
+    assert MatchUnmatch_calculate_the_match("Superlikes", "Likes") == "Match"
+    assert MatchUnmatch_calculate_the_match("Superlikes", "Dislikes") == "NoMatch"
+    assert MatchUnmatch_calculate_the_match("Superlikes", "Superlikes") == "Match"
+    assert MatchUnmatch_calculate_the_match("Dislikes", "Superlikes") == "NoMatch"
+    assert MatchUnmatch_calculate_the_match("Dislikes", "Likes") == "NoMatch"
+    assert MatchUnmatch_calculate_the_match("Dislikes", "Dislikes") == "NoMatch"
+
+
+def test_MatchUnmatch_calculate_the_match_failure():
+    
+    assert MatchUnmatch_calculate_the_match(42, "Likes") == False
+    
+    
+@pytest.mark.asyncio
+async def test_MatchUnmatch_delete_record_from_redis_success():
+    
+    userId1='UserId123'
+    userId2='UserId234'
+    childCollectionName=None
+
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.redis_client') as mock_redis_client:
+            mock_redis_client.smembers.return_value = ['UserId234']
+            # Mocking redis_client by fetching the scard value
+            allUserMatchOrUnmatches = list(mock_redis_client.smembers.return_value)
+            result = await MatchUnmatch_delete_record_from_redis(user_id_1=userId1, user_id_2= None, childCollectionName=childCollectionName)
+            assert result == True
+
+
+@pytest.mark.asyncio
+async def test_MatchUnmatch_delete_record_from_redis_failure():
+    
+    userId1='UserId123'
+    userId2='UserId234'
+    childCollectionName=None
+
+    with patch('Gateways.MatchUnmatchGateway.async_db') as mock_async_db:
+        with patch('Gateways.MatchUnmatchGateway.redis_client') as mock_redis_client:
+            mock_redis_client.smembers.side_effect = Exception("No value")
+            # Mocking redis_client by fetching the scard value
+            allUserMatchOrUnmatches = list(mock_redis_client.smembers.return_value)
+            result = await MatchUnmatch_delete_record_from_redis(user_id_1=userId1, user_id_2= None, childCollectionName=childCollectionName)
+            assert result == False
